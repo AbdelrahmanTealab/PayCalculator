6dc958139164f6b106c122361d896b258793f11a
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2021-02-05 20:46:29 -0500

    add hardware back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(
+    private platform: Platform,
+    private alertController: AlertController,
+  ) {
+    this.initializeApp();
+    this.backButtonEvent();
+  }
+
+  initializeApp(){
+    this.platform.ready().then(()=>{
+    })
+  }
+
+  backButtonEvent(){
+    this.platform.backButton.subscribeWithPriority(10,()=>{
+      this.backButtonAlert();
+    })
+  }
+
+  async backButtonAlert(){
+    const alert = await this.alertController.create({
+      message:'Back button pressed !'
+    });
+    await alert.present();
+  }
 }
