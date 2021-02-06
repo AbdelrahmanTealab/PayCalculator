48a74e35298b1679e6089c6906180a33776ef34e
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2021-02-06 01:44:40 -0500

    pass values to function calculate

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  regularPay = 0
+  overtimePay = 0
+  tax = 0
+  totalPay = 0
+  hrlyRate:number
+  hrsWorked:number
   constructor() {}
+
+  calculate(hrsWorked:number, hrlyRate:number){
+
+    if (hrsWorked <= 40) {
+      this.regularPay = hrsWorked * hrlyRate
+      this.overtimePay = 0
+      this.totalPay = this.regularPay + this.totalPay
+    } else {
+      this.regularPay = 40 * hrlyRate
+      this.overtimePay = (hrsWorked-40)*hrlyRate*1.5 + 40*hrlyRate
+      this.totalPay = this.regularPay + this.totalPay
+    }
+    this.tax = this.totalPay*0.18
+  }
 
 }
