112174151bf1e4e6b719732b15775f0b4c6732d9
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2021-02-06 02:12:04 -0500

    implement calculate function and round up output to 2 and 1 decimals

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,22 +11,27 @@ export class HomePage {
   overtimePay = 0
   tax = 0
   totalPay = 0
+  net = 0
   hrlyRate:number
   hrsWorked:number
   constructor() {}
 
   calculate(hrsWorked:number, hrlyRate:number){
 
+    this.totalPay = 0
+    this.tax = 0
+
     if (hrsWorked <= 40) {
       this.regularPay = hrsWorked * hrlyRate
       this.overtimePay = 0
-      this.totalPay = this.regularPay + this.totalPay
+      this.totalPay = this.regularPay + this.overtimePay
     } else {
       this.regularPay = 40 * hrlyRate
-      this.overtimePay = (hrsWorked-40)*hrlyRate*1.5 + 40*hrlyRate
-      this.totalPay = this.regularPay + this.totalPay
+      this.overtimePay = (hrsWorked-40)*hrlyRate*1.5
+      this.totalPay = this.regularPay + this.overtimePay
     }
     this.tax = this.totalPay*0.18
+    this.net = this.totalPay - this.tax
   }
 
 }
